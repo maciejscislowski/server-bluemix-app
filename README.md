81ff85feec3606fe9a5de44b2d415b7e64428189
maciejscislowski <Maciek@MacBook-Pro-Maciek.local>
2016-01-29 11:18:24 +0100

    initial commit with mvn deploy to the Bluemix

---
+++ README.md
@@ -0,0 +1,5 @@
+# server-bluemix-app
+
+## Build and deploy on the Bluemix
+
+mvn clean package -P bluemix
---
+++ src/main/java/it/scislowski/bluemix/server/ServerBluemixAppApplication.java
@@ -0,0 +1,12 @@
+package it.scislowski.bluemix.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerBluemixAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerBluemixAppApplication.class, args);
+	}
+}
